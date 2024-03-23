38fa2ea8d2129e2f7a57b17d3c57571f4ca7dee7
mohamedebrahimkhatab <mohamedebrahimkhatab@gmail.com>
2024-03-23 03:04:56 +0200

    change fullname to first and last name

--- Vezeeta.Core/Contracts/DoctorDtos/GetIdDoctorDto.cs
+++ Vezeeta.Core/Contracts/DoctorDtos/GetIdDoctorDto.cs
@@ -3,7 +3,8 @@
 public class GetIdDoctorDto
 {
     public string? PhotoPath { get; set; }
-    public string? FullName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public string? Email { get; set; }
     public string? Phone { get; set; }
     public string? Specialize { get; set; }
